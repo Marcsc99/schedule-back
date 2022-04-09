fd4aef30a1b2e5a1fabc328850f69a6e820fb589
Marc Sanchez <marc.sanchez.civantos@gmail.com>
2022-04-09 23:07:01 +0200

    cors fic

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ import bodyParser from 'body-parser';
 const app = express();
 import routes from './routes/index.js';
 
-app.use(cors({
-    origin: '*',
-    optionsSuccessStatus: 200
-}));
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 
